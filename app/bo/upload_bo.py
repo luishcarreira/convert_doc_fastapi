6079a0e0fc11565d9a28e98f9d8c914661bd8f2b
Luis Henrique Carreira <luishenriquecarreira@gmail.com>
2023-06-30 22:47:46 -0300

    first commit

---
+++ app/bo/upload_bo.py
@@ -0,0 +1,121 @@
+import os
+import subprocess
+import re
+
+from PyPDF2 import PdfReader, PdfWriter, PdfFileReader
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch, cm
+from reportlab.lib import colors
+from reportlab.lib.utils import ImageReader
+from reportlab.lib.pagesizes import A4
+
+class UploadBo:
+
+    """async def replaceString(arquivo_request: ArquivoRequest) -> str:
+        with open("arquivo.docx", "wb") as arquivo_temporario:
+            arquivo_temporario.write(arquivo_request.file)
+
+        doc = Document("arquivo.docx")
+
+        footer_tables = []
+
+        for section in doc.sections:
+            footer = section.footer
+            if footer is not None:
+                for table in footer.tables:
+                    footer_tables.append(table)
+
+        cells = []
+
+        for table in footer_tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    cells.append(cell.text)
+
+        os.remove("arquivo.docx")
+
+        return cells"""
+
+
+    async def convertToPdf(file: str):
+        '''caminho_docx = "arquivo.docx"
+
+        with open(caminho_docx, "wb") as arquivo_temporario:
+            shutil.copyfileobj(file, arquivo_temporario)
+
+        convert(caminho_docx)
+
+        os.remove(caminho_docx)'''
+
+
+    async def makeWatermark():
+
+        # text
+        '''pdf = canvas.Canvas("watermark.pdf", pagesize=A4)
+        pdf.translate(inch, inch)
+        pdf.setFillColor(colors.grey, alpha=0.6)
+        pdf.setFont("Helvetica", 50)
+        pdf.rotate(45)
+        pdf.drawCentredString(400, 100, 'SUPER HIPER Marca')
+        pdf.save()'''
+
+        # imagem
+        img = ImageReader('logo.jpg')
+        pdf = canvas.Canvas("watermark.pdf", pagesize=A4)
+        pdf.setFillColor(colors.grey, alpha=0.6)
+        img_width, img_height = img.getSize()
+        aspect = img_height / float(img_width)
+        display_width = 380
+        display_height = (display_width * aspect)
+        pdf.drawImage('logo.jpg',
+                        (4*cm),
+                        ((29.7 - 5) * cm) - display_height,
+                        width=display_width,
+                        height=display_height,
+                        mask="auto")
+        pdf.save()
+
+
+    async def addWaterMark(path: str) -> bytes:
+
+        await UploadBo.makeWatermark()
+
+        reader = PdfReader(path)
+        page_indices = list(range(0, len(reader.pages)))
+
+        writer = PdfWriter()
+
+        for i in page_indices:
+            content_page = reader.pages[i]
+
+            reader_stamp = PdfReader("watermark.pdf")
+            image_page = reader_stamp.pages[0]
+
+            image_page.merge_page(content_page)
+            writer.add_page(image_page)
+
+        with open(path, "wb") as fp:
+            writer.write(fp)
+
+        with open(path, "rb") as arquivo_pdf:
+            conteudo_pdf = arquivo_pdf.read()
+
+        return conteudo_pdf
+    
+
+    async def convert_to(arquivo_request: ArquivoRequest) -> str:
+        with open("arquivo.docx", "wb") as arquivo_temporario:
+            arquivo_temporario.write(arquivo_request.file)
+
+        args = ['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', '.', 'arquivo.docx']
+        
+        process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=15)
+        filename = re.search('-> (.*?) using filter', process.stdout.decode())
+        
+        retorno = await UploadBo.addWaterMark(filename.group(1))
+ 
+        os.remove(filename.group(1))
+        os.remove("arquivo.docx")
+        os.remove("watermark.pdf")
+
+        return retorno
---
+++ app/main.py
@@ -0,0 +1,136 @@
+import sys
+import os
+import subprocess
+import re
+
+from fastapi import FastAPI, File, UploadFile, Response, status
+from pydantic import BaseModel
+from typing import List
+
+from PyPDF2 import PdfReader, PdfWriter, PdfFileReader
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch, cm
+from reportlab.lib import colors
+from reportlab.lib.utils import ImageReader
+from reportlab.lib.pagesizes import A4
+
+#from upload_bo import UploadBo
+
+app = FastAPI()
+
+
+class Tags(BaseModel):
+    tag: str
+    valor: str
+
+class ListTags(BaseModel):
+    tags: List[Tags]
+
+
+async def makeWatermark():
+
+    # text
+    '''pdf = canvas.Canvas("watermark.pdf", pagesize=A4)
+    pdf.translate(inch, inch)
+    pdf.setFillColor(colors.grey, alpha=0.6)
+    pdf.setFont("Helvetica", 50)
+    pdf.rotate(45)
+    pdf.drawCentredString(400, 100, 'SUPER HIPER Marca')
+    pdf.save()'''
+
+    # imagem
+    img = ImageReader('logo.jpg')
+    pdf = canvas.Canvas("watermark.pdf", pagesize=A4)
+    pdf.setFillColor(colors.grey, alpha=0.6)
+    img_width, img_height = img.getSize()
+    aspect = img_height / float(img_width)
+    display_width = 380
+    display_height = (display_width * aspect)
+    pdf.drawImage('logo.jpg',
+                    (4*cm),
+                    ((29.7 - 5) * cm) - display_height,
+                    width=display_width,
+                    height=display_height,
+                    mask="auto")
+    pdf.save()
+
+
+async def addWaterMark(path: str) -> bytes:
+
+    await makeWatermark()
+
+    reader = PdfReader(path)
+    page_indices = list(range(0, len(reader.pages)))
+
+    writer = PdfWriter()
+
+    for i in page_indices:
+        content_page = reader.pages[i]
+
+        reader_stamp = PdfReader("watermark.pdf")
+        image_page = reader_stamp.pages[0]
+
+        image_page.merge_page(content_page)
+        writer.add_page(image_page)
+
+    with open(path, "wb") as fp:
+        writer.write(fp)
+
+    with open(path, "rb") as arquivo_pdf:
+        conteudo_pdf = arquivo_pdf.read()
+
+    return conteudo_pdf
+
+
+async def convert_to(file: bytes) -> str:
+    with open("arquivo.docx", "wb") as arquivo_temporario:
+        arquivo_temporario.write(file)
+
+    args = ['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', '.', 'arquivo.docx']
+
+    process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=15)
+    filename = re.search('-> (.*?) using filter', process.stdout.decode())
+
+    retorno = await addWaterMark(filename.group(1))
+
+    os.remove(filename.group(1))
+    os.remove("arquivo.docx")
+    os.remove("watermark.pdf")
+
+    return retorno
+        
+
+@app.get('/')
+def hello_world():
+    return {'message': 'Hello, World!'}
+
+@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+def perform_healthcheck():
+    return {'healthcheck': sys.platform}
+
+@app.post('/api/testtags', status_code=status.HTTP_200_OK)
+def test_tags(tags: ListTags):
+    return {'tags': tags}
+
+@app.post('/api/docx2pdf', status_code=status.HTTP_200_OK)
+async def docx2pdf(file: UploadFile = File(...)):
+    arquivo = file.file.read()
+
+    retorno = await convert_to(arquivo)
+
+    return Response(content=retorno,
+                    media_type="application/pdf",
+                    headers={"Content-Disposition": "attachment; filename=arquivo.pdf"})
+
+
+"""@app.post('/api/upload/replace_string')
+async def replace_string(file: UploadFile = File(...)):
+    arquivo_request: ArquivoRequest = ArquivoRequest()
+
+    arquivo_request.nome = file.filename
+    arquivo_request.file = file.file.read()
+    arquivo_request.tags = None
+
+    retorno = await UploadBo.replaceString(arquivo_request)
+
+    return retorno"""
